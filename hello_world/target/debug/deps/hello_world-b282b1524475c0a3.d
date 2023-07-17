fe8dd3c94a9352bc2f270ffb328ac76fb3d94eb1
Yao Jie <yaoojie@foxmail.com>
2023-07-17 08:47:04 +0800

    update

---
+++ hello_world/target/debug/deps/hello_world-b282b1524475c0a3.d
@@ -0,0 +1,5 @@
+/home/yaojie/workspace/code/LearnRust/hello_world/target/debug/deps/hello_world-b282b1524475c0a3.rmeta: src/main.rs
+
+/home/yaojie/workspace/code/LearnRust/hello_world/target/debug/deps/hello_world-b282b1524475c0a3.d: src/main.rs
+
+src/main.rs:
